1840dcb2e4748e971046c7b5ad42ff607e851e64
ohseungmin <ohmin2886@gmail.com>
2023-12-15 23:18:37 +0900

    조건식과 타입 확인으로 나누기가 가능한 정적 함수 선언 방식

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // 정적인 함수 선언 방식
 
-function three(number) {
-  return 10;
+function divide(number) {
+  if(typeof(number) === "number") {
+    return `${number / 10}`;
+  }
 }
 
-const threeResult = three()
-console.log(threeResult)
+const divideResult = divide(100)
+console.log(divideResult)
